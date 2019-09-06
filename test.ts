10cecc2d663d656bde9e04a6f64e4e4684cb3517
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:53 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,41 @@
+import sortBy from "./index.ts";
+
+
+test("works", () => {
+  expect(sortBy(({id}) => id)([{id: 3}, {id: 1}, {id: 2}])).toEqual([{id: 1}, {id: 2}, {id: 3}]);
+
+  t.done();
+});
+
+test("works", () => {
+  expect(sortBy(({id}) => id)([{id: 1}, {id: 2}, {id: 3}])).toEqual([{id: 1}, {id: 2}, {id: 3}]);
+
+  t.done();
+});
+
+test("works", () => {
+  expect(sortBy(({id}) => id)([{id: 1}, {id: 2}, {id: 1}])).toEqual([{id: 1}, {id: 1}, {id: 2}]);
+
+  t.done();
+});
+
+
+test("works", () => {
+  expect(
+    sortBy(({id}) => id)([{id: "ad2e123"}, {id: "123123dd"}, {id: "asd2o3o1"}])
+  ).toEqual([{id: "123123dd"}, {id: "ad2e123"}, {id: "asd2o3o1"}]);
+
+  t.done();
+});
+
+test("works", () => {
+  expect(sortBy(({id}) => id)([{id: true}, {id: false}, {id: true}])).toEqual([{id: false}, {id: true}, {id: true}]);
+
+  t.done();
+});
+
+test("works", () => {
+  expect(sortBy(({length}) => length)(["ac", "b"])).toEqual(["b", "ac"]);
+
+  t.done();
+});
